d5a2eb6a9d479ede0fc9c3f6d251c6534838a864
mohammadosa <mohamedsiddig5026485@gmail.com>
2022-01-26 11:27:06 +0200

    mongoose start!
    decided to change the database to for cloud benefits.
    deleted some files as a start
    going to return after getting done from redis cache.

--- src/prisma resources/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-/**Created this module in order to make auth.model able to use Prisma services, make everything in a file next time! */
-import { PrismaService } from './prisma.service';
-import { Module } from '@nestjs/common';
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}

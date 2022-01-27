24503047840cc3c59b16f53d1ba470c92bcbc333
mohammadosa <mohamedsiddig5026485@gmail.com>
2022-01-27 11:44:22 +0200

    mongoose
    added nestjs mongoose package
    going to learn mongodb and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaClient } from '@prisma/client';
 import { AuthModule } from './auth/auth.module';
-import { UserService } from './user/user.service';
-import { PrismaService } from './prisma resources/prisma.service';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma resources/prisma.module';
 import { AuthService } from './auth/auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule, PrismaClient, UserModule, PrismaModule],
+  imports: [AuthModule, MongooseModule.forRoot(process.env.DATABASE_URL) ],
   controllers: [AppController],
-  providers: [AppService, UserService, PrismaService, AuthService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
